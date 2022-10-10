4cdd7a31b955c159ee044338df954beb664e4953
Exe Cieri <execieri@gmail.com>
2022-10-10 13:05:27 -0300

    agregando objetos, arrays, funciones y dom

--- js/script.js
+++ js/script.js
@@ -54,3 +54,120 @@
 //     console.log(i)
 // }
 
+// let total = 0
+// function acumular(num){
+//     total += num
+// }
+// function porCadaUno(array, fn){
+    // for( const elemento of array) {fn(elemento)}
+// }
+// porCadaUno([1,3,4,5], acumular)
+// console.log(total)
+
+function Producto(nombre, precio, stock, cat, img){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.cat = cat
+    this.img = img
+    this.restarStock = function(cantidad){
+        this.stock -= cantidad
+    }
+}
+let productoA = new Producto("performance", 100, 50, "royal", "./img/performance3.jpg")
+let productoB = new Producto("eukanuba", 90, 40,"purina", "./img/eukanuba.jpg")
+let productoC = new Producto("proplan", 50, 20, "purina", "./img/proplan.jpg")
+let productoD = new Producto("performance junior", 50, 30,"royal", "./img/performancejunior.jpg")
+let productoE = new Producto("excellent", 30, 15,"purina", "./img/excellent.jpg")
+let productoF = new Producto("puppy", 50, 20, "royal", "./img/puppy.jpg")
+
+let precioTotal = 0
+let listaProductos = [productoA, productoB, productoC, productoD, productoE, productoF]
+let listaProductosConStock = listaProductos.filter((prod) => prod.stock > 0)
+let listaNombres = listaProductosConStock.map((prod) => prod.nombre)
+
+let catalogo = document.getElementById("catalogo")
+
+function render (lista){
+    catalogo.innerHTML = ""
+
+    for (const prod of lista){
+
+    let card = document.createElement("div")
+    card.className = "card"
+    card.innerHTML = `<H2>${prod.nombre}</H2><p>precio:$${prod.precio}</p> <img src= ${prod.img}> </img>`
+    catalogo.append(card)}
+}
+// ----------NO PUEDO MOSTRAR TODOS LOS PRODUCTOS JUNTOS SIN CLASIFICARLOS POR SU CATEGORIA------------------
+
+render (listaProductosConStock)
+alert ("tenemos productos royal y purina")
+let categoria = prompt ("ingrese que marca quiere ver:")
+let filtroActual = listaProductos.filter((prod) => prod.cat == categoria)
+
+render(filtroActual)
+
+// alert("estos son nuestros productos:  \n - " + listaNombres.join("\n -"))
+
+// let cantidadCompra = prompt("que cantidad de productos distintos quiere comprar")
+
+
+
+function calculoPrecio(cantidad, precio){
+    precioTotal += (cantidad * precio)
+}
+
+function calculstock(cantidad, stock, precio){
+    if(cantidad <= stock){
+        calculoPrecio(cantidad, precio)
+   }
+   else{
+    alert("actualmente tenemos" + stock + "unidades de este producto")}
+}
+
+
+
+for (let i = 0; i < cantidadCompra; i = i + 1){
+    
+    let productoCompra = prompt("ingrese que productos quiere comprar:\n - performance \n - eukanuba \n - proplan \n - performance junior \n - excellent \n -puppy")
+
+    if (productoCompra == "performance"){
+        let cantidadProductoPerformance = prompt("ingrese que cantidad de " + productoA.nombre + " desea comprar")     
+       calculstock(cantidadProductoPerformance, productoA.stock, productoA["precio"])
+       productoA.restarStock(cantidadProductoPerformance)
+    }
+    else if (productoCompra == "eukanuba"){
+        let cantidadProductoB = prompt("ingrese que cantidad de " + productoB.nombre + " desea comprar")
+        calculstock (cantidadProductoB, productoB.stock, productoB["precio"])
+        productoB.restarStock(cantidadProductoB)
+    }
+    else if (productoCompra == "proplan"){
+        let cantidadProductoC = prompt("ingrese que cantidad de " + productoC.nombre + " desea comprar")
+        calculstock(cantidadProductoC, productoC.stock, productoC["precio"])
+        productoC.restarStock(cantidadProductoC) 
+    }
+    else if(productoCompra == "performance junior"){
+        let cantidadProductoD = prompt ("ingrese que cantidad de " + productoD.nombre + " desea comprar")
+       calculstock(cantidadProductoD, productoD.stock, productoD["precio"])
+       productoD.restarStock(cantidadProductoD)
+    }
+    else if (productoCompra == "excellent"){
+            let cantidadProductoE = prompt ( "ingrese que cantidad de " + productoE.nombre + " desea comprar")
+        calculstock(cantidadProductoE, productoE.stock, productoE["precio"])
+        productoE.restarStock(cantidadProductoE)
+    }
+    else if (productoCompra == "puppy"){
+        let cantidadProductoF = prompt ( "ingrese que cantidad de " + productoF.nombre + " desea comprar")
+    calculstock(cantidadProductoF, productoF.stock, productoF["precio"])
+    productoF.restarStock(cantidadProductoF)
+    }
+
+    else {
+    alert("no tenemos ese producto en venta")
+    }
+}
+if(precioTotal != 0){
+    alert("el precio total es:" + precioTotal)
+}
+
+alert("Gracias por su visita")
--- js/script2.js
+++ js/script2.js
@@ -1,62 +1,89 @@
-let nombreProductoA= "performance"
-let precioProductoA= 200
-let stockProductoA= 15
+// let producatoA = {
+//     nombre: 
+// }
 
-let nombreProductoB="eukanuba"
-let precioProductoB= 150
-let stockProductoB= 20
 
-let nombreProductoC = "sieger"
-let precioProductoC = 100
-let stockProductoC= 10
+// let nombreProductoA= "performance"
+// let precioProductoA= 200
+// let stockProductoA= 15
 
-let nombreProductoD = "canfeed"
-let precioProductoD = 50
-let stockProductoD = 10
+// let nombreProductoB="eukanuba"
+// let precioProductoB= 150
+// let stockProductoB= 20
 
-let nombreProductoE = "pedigree"
-let precioProductoE = 30
-let stockProductoE = 10
+// let nombreProductoC = "sieger"
+// let precioProductoC = 100
+// let stockProductoC= 10
+
+// let nombreProductoD = "canfeed"
+// let precioProductoD = 50
+// let stockProductoD = 10
+
+// let nombreProductoE = "pedigree"
+// let precioProductoE = 30
+// let stockProductoE = 10
+
+function Producto(nombre, precio, stock){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.restarStock = function(cantidad){
+        this.stock -= cantidad
+    }
+}
+let productoA = new Producto("performance", 100, 50)
+let productoB = new Producto("eukanuba", 90, 40)
+let productoC = new Producto("sieger", 50, 20)
+let productoD = new Producto("canfeed", 50, 20)
+let productoE = new Producto("pedigree", 30, 15)
 
 let precioTotal = 0
+let listaProductos = [productoA, productoB, productoC, productoD, productoE]
+let listNombres = listaProductos.filter((prod) => prod.stock > 0)
 
 alert("estos son nuestros productos:  \n - performance \n - eukanuba \n - sieger \n - canfeed \n - pedigree ")
 
 let cantidadCompra = prompt("que cantidad de productos distintos quiere comprar")
 
+
+
+function calculoPrecio(cantidad, precio){
+    precioTotal += (cantidad * precio)
+}
+
+function calculstock(cantidad, stock, precio){
+    if(cantidad <= stock){
+        calculoPrecio(cantidad, precio)
+   }
+   else{
+    alert("actualmente tenemos" + stockProductoA + "unidades de este producto")}
+}
+
+
+
 for (let i = 0; i < cantidadCompra; i = i + 1){
     
     let productoCompra = prompt("ingrese que productos quiere comprar:\n - performance \n - eukanuba \n - sieger \n - canfeed \n - pedigree")
 
-
     if (productoCompra.toUpperCase() == "performance"){
-        let cantidadProductoPerformance = prompt("ingrese que cantidad de " + nombreProductoA + " desea comprar")
-        if(cantidadProductoPerformance <=stockProductoA){
-             precioTotal = precioTotal + (cantidadProductoA * precioProductoA)
-        }
-        else{
-        alert("actualmente tenemos" + stockProductoA + "unidades de este producto")}
+        let cantidadProductoPerformance = prompt("ingrese que cantidad de " + nombreProductoA + " desea comprar")     
+       calculstock(cantidadProductoPerformance, stockProductoA, precioProductoA)
     }
-
-
     else if (productoCompra == "eukanuba"){
         let cantidadProductoB = prompt("ingrese que cantidad de " + nombreProductoB + " desea comprar")
-        precioTotal = precioTotal + (cantidadProductoB * precioProductoB)
-   
+        calculstock (cantidadProductoB, stockProductoB, precioProductoB)
     }
-
     else if (productoCompra == "sieger"){
         let cantidadProductoC = prompt("ingrese que cantidad de " + nombreProductoC + " desea comprar")
-        precioTotal = precioTotal + (cantidadProductoC * precioProductoC)
-    
+        calculstock(cantidadProductoC, stockProductoC, precioProductoC) 
     }
     else if(productoCompra == "canfeed"){
         let cantidadProductoD = prompt ("ingrese que cantidad de " + nombreProductoD + " desea comprar")
-        precioTotal = precioTotal + (cantidadProductoD * precioProductoD)
+       calculstock(cantidadProductoD, stockProductoD, precioProductoD)
     }
     else if (productoCompra == "pedigree"){
             let cantidadProductoE = prompt ( "ingrese que cantidad de " + nombreProductoE + " desea comprar")
-            precioTotal = precioTotal + (cantidadProductoE * precioProductoE)
+        calculstock(cantidadProductoE, stockProductoE, precioProductoE)
     }
 
     else {
